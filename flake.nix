46fa55d2f225d22f81b48c73a5cf3a94a811463d
Michael Zappa <me@michzappa.com>
2022-04-14 15:15:10 -0400

    update devShell to include ssl library

--- flake.nix
+++ flake.nix
@@ -16,22 +16,9 @@
         });
       in {
         devShell = pkgs.mkShell {
-          buildInputs = with pkgs;
-            [
-              sbcl
-              # TODO might need more dependencies eventually
-              # openssl
-              # z3
-              # z3.lib
-              # gcc
-              # pkg-config
-              # zlib
-              # clang
-              # for presentation
-              # texlive.combined.scheme-full
-            ];
-          # LD_LIBRARY_PATH =
-          # "${pkgs.openssl}:${pkgs.stdenv.cc.cc.lib}:${pkgs.stdenv.cc.cc.lib}/lib:${pkgs.z3.lib}/lib:${pkgs.z3.lib}:$LD_LIBRARY_PATH";
+          buildInputs = with pkgs; [ sbcl openssl ];
+          LD_LIBRARY_PATH =
+            "${pkgs.openssl.out}/lib:$LD_LIBRARY_PATH";
         };
       });
 }
