4de0d0f1e84301bc964f4acb2260490dbbb03ef1
Michael Zappa <me@michzappa.com>
2022-05-11 17:42:06 -0400

    remove direnv, move to 'nix develop'

--- flake.nix
+++ flake.nix
@@ -18,6 +18,13 @@
         devShell = pkgs.mkShell {
           buildInputs = with pkgs; [ sbcl openssl parallel ];
           LD_LIBRARY_PATH = "${pkgs.openssl.out}/lib";
+          shellHook = ''
+            export PATH=$PWD/bin:$PATH
+            export ACL2_SYSTEM_BOOKS=$PWD/acl2/books
+            export ACL2S_SCRIPTS=$PWD/scripts
+            export ACL2_LISP=sbcl
+            export ACL2S_NUM_JOBS=8
+          '';
         };
       });
 }
